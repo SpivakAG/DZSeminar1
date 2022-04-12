6d9bad66bc7306f98d1998033d7de8ca31a2a9e2
ArtemSpivak <spivak25@yandex.ru>
2022-04-12 19:08:43 +0300

    задача решена

--- Program.cs
+++ Program.cs
@@ -1,1 +1,18 @@
-﻿
+﻿Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number >= 100)
+{
+    if (number < 1000)
+    {
+        int result = number % 10;
+        Console.WriteLine(result);
+    }
+    else
+    {
+        Console.WriteLine("Введенное число не является трехзначным");
+    }
+}
+else 
+{
+    Console.WriteLine("Введенное число не является трехзначным");
+}
